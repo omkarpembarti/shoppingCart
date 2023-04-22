716d374bece7c42762415980f41eb90275c02884
OmkarPembarti <omkarpembarti@gmail.com>
2023-04-22 13:37:30 +0530

    first commit dinesh

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Cart from "./component/Cart";
 import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
+
+  console.log("First commit");
   return (
     <BrowserRouter>
       <Header />
